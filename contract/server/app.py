b84c0077a62fb73b5edf5b8193625b0ad0e907af
DeanWay <deanway93@gmail.com>
2020-01-27 11:01:58 -0500

    simplify coors

--- contract/server/app.py
+++ contract/server/app.py
@@ -33,14 +33,10 @@ class Query(graphene.ObjectType):
 
 
 app = FastAPI()
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
